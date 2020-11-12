1b77df35735caf9f2f7fc799457ab9e3d352b450
Booker-M <bookerjmartin@gmail.com>
2020-11-12 11:15:04 -0800

    Updates

---
+++ precache-manifest.e6ba280915727555f272cd9feb2ff94d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4135cda244a178be1adb3fd5eb58f8b",
+    "url": "/parley/index.html"
+  },
+  {
+    "revision": "fa9f1c8c6b7c276114c2",
+    "url": "/parley/static/css/2.ffcf707b.chunk.css"
+  },
+  {
+    "revision": "c6575bae2844a565236f",
+    "url": "/parley/static/css/main.fdf4ba7c.chunk.css"
+  },
+  {
+    "revision": "fa9f1c8c6b7c276114c2",
+    "url": "/parley/static/js/2.107105fa.chunk.js"
+  },
+  {
+    "revision": "cbf44c833c3730baea337719e3cdf4f4",
+    "url": "/parley/static/js/2.107105fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6575bae2844a565236f",
+    "url": "/parley/static/js/main.409f5f9b.chunk.js"
+  },
+  {
+    "revision": "3317e6a73dd6566ddb7f",
+    "url": "/parley/static/js/runtime-main.e1770dcc.js"
+  },
+  {
+    "revision": "9299706f23346da2776f4aef1e52996a",
+    "url": "/parley/static/media/ic_wave_hand.9299706f.png"
+  },
+  {
+    "revision": "477fc8ddd40d200fe8f2635b5151a873",
+    "url": "/parley/static/media/mimi1.477fc8dd.gif"
+  },
+  {
+    "revision": "82a783353a27abe59feafd40bc686af1",
+    "url": "/parley/static/media/mimi2.82a78335.gif"
+  },
+  {
+    "revision": "477fc8ddd40d200fe8f2635b5151a873",
+    "url": "/parley/static/media/mimi3.477fc8dd.gif"
+  },
+  {
+    "revision": "6b65742393c65511ba223ec388c85ecc",
+    "url": "/parley/static/media/mimi4.6b657423.gif"
+  },
+  {
+    "revision": "609127cb5129f8e218fb57f059497c87",
+    "url": "/parley/static/media/mimi5.609127cb.gif"
+  },
+  {
+    "revision": "1ce35a6b3c21cc308a7683dfbea81c63",
+    "url": "/parley/static/media/mimi6.1ce35a6b.gif"
+  },
+  {
+    "revision": "ace546bd302229af3a0c7e7221d2dc10",
+    "url": "/parley/static/media/mimi7.ace546bd.gif"
+  },
+  {
+    "revision": "6646a3dec849498313a17c7de5d2ea5c",
+    "url": "/parley/static/media/mimi8.6646a3de.gif"
+  },
+  {
+    "revision": "b5ec7c32285b24c09eca4646a4511518",
+    "url": "/parley/static/media/mimi9.b5ec7c32.gif"
+  },
+  {
+    "revision": "59c3edcbfe82395f6156f725473d42bb",
+    "url": "/parley/static/media/partyParrot.59c3edcb.gif"
+  }
+]);
