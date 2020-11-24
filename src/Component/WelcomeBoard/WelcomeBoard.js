45ae595489dccb944e0d2e8d02e3952e9a42391d
andrewseamanco <andrew.seamanco@gmail.com>
2020-11-24 16:34:28 -0700

    Make welcome board functional

--- src/Component/WelcomeBoard/WelcomeBoard.js
+++ src/Component/WelcomeBoard/WelcomeBoard.js
@@ -2,22 +2,38 @@ import React, {Component} from 'react'
 import 'react-toastify/dist/ReactToastify.css'
 import './WelcomeBoard.css'
 
-export default class WelcomeBoard extends Component {
-    render() {
-        return (
-            <div className="viewWelcomeBoard">
-        <span className="textTitleWelcome">{`Welcome, ${
-            this.props.currentUserNickname
-            }`}</span>
-                <img
-                    className="avatarWelcome"
-                    src={this.props.currentUserAvatar}
-                    alt="icon avatar"
-                />
-                <span className="textDesciptionWelcome">
-          Explore the world through conversation.
-        </span>
-            </div>
-        )
-    }
+export default function WelcomeBoard(props) {
+    return (
+        <div className="viewWelcomeBoard">
+            <span className="textTitleWelcome">{`Welcome, ${props.currentUserNickname}`}</span>
+            <img
+                className="avatarWelcome"
+                src={props.currentUserAvatar}
+                alt="icon avatar"
+            />
+            <span className="textDescriptionWelcome">
+                Explore the world through conversation.
+            </span>
+        </div>
+    )
 }
+
+// export default class WelcomeBoard extends Component {
+//     render() {
+//         return (
+//             <div className="viewWelcomeBoard">
+//         <span className="textTitleWelcome">{`Welcome, ${
+//             this.props.currentUserNickname
+//             }`}</span>
+//                 <img
+//                     className="avatarWelcome"
+//                     src={this.props.currentUserAvatar}
+//                     alt="icon avatar"
+//                 />
+//                 <span className="textDesciptionWelcome">
+//           Explore the world through conversation.
+//         </span>
+//             </div>
+//         )
+//     }
+// }
